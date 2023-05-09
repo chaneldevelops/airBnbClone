abd6a50d9ad9c6958c17c21123cf3ebc4d8f5e31
chaneldevelops <chanel.developsdesigns@gmail.com>
2023-05-09 14:26:15 -0400

    Experiences updated
    - added and updated photos
    - currently 8 experiences

--- src/data.jsx
+++ src/data.jsx
@@ -14,29 +14,16 @@ export default [
     },
     {
         id: 2,
-        title: "Learn Wedding Photography",
-        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
-        price: 125,
-        coverImg: "wedding-photography.png",
+        title: "Sushi Making 101",
+        description: "Learn how to make mouthwatering sushi rolls in our hands-on class, where you'll discover the art of rolling, seasoning, and plating to perfection",
+        price: 60,
+        coverImg: "sushi-making.jpg",
         stats: {
             rating: 5.0,
-            reviewCount: 30
+            reviewCount: 45
         },
         location: "Online",
-        openSpots: 27,
-    },
-    {
-        id: 3,
-        title: "Group Mountain Biking",
-        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
-        price: 50,
-        coverImg: "mountain-bike.png",
-        stats: {
-            rating: 4.8,
-            reviewCount: 2
-        },
-        location: "Norway",
-        openSpots: 3,
+        openSpots: 16,
     },
     {
         id: 3,
@@ -50,5 +37,70 @@ export default [
         },
         location: "Orlando, FL",
         openSpots: 12,
+    },
+    {
+        id: 4,
+        title: "Group Mountain Biking",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "mountain-bike.png",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "Plant City, FL",
+        openSpots: 3,
+    },
+    {
+        id: 5,
+        title: "Canvas and Color: Group Painting Class",
+        description: "Unleash your creativity and learn the fundamentals of painting in our beginner-friendly class, where our expert instructor will guide you through the process of creating your own beautiful artwork step-by-step.",
+        price: 35,
+        coverImg: "painting-class.jpg",
+        stats: {
+            rating: 4.5,
+            reviewCount: 23
+        },
+        location: "Tampa, FL",
+        openSpots: 6,
+    },
+    {
+        id: 6,
+        title: "Acting 101",
+        description: "Unleash your creativity and learn the fundamentals of painting in our beginner-friendly class, where our expert instructor will guide you through the process of creating your own beautiful artwork step-by-step.",
+        price: 20,
+        coverImg: "acting-classes.jpg",
+        stats: {
+            rating: "4.0",
+            reviewCount: 5
+        },
+        location: "Tampa, FL",
+        openSpots: 2,
+    },
+    {
+        id: 7,
+        title: "The Spectrum Museum of Color and Light",
+        description: "Immerse yourself in a world of vibrant color and light at The Spectrum Museum, where dazzling exhibits and interactive displays ignite your imagination and inspire your creativity.",
+        price: 31,
+        coverImg: "museum .jpg",
+        stats: {
+            rating: "5.0",
+            reviewCount: 28
+        },
+        location: "St.Petersburg, FL",
+        openSpots: "available",
+    },
+    {
+        id: 8,
+        title: "The Spectrum Museum of Color and Light",
+        description: "Immerse yourself in a world of vibrant color and light at The Spectrum Museum, where dazzling exhibits and interactive displays ignite your imagination and inspire your creativity.",
+        price: 17,
+        coverImg: "circus.jpg",
+        stats: {
+            rating: 4.9,
+            reviewCount: 46
+        },
+        location: "Sarasota, FL",
+        openSpots: 15,
     }
 ]
