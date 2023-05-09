a5ae48f792260c11329b7e473c01f0b06aa9f50d
chaneldevelops <chanel.developsdesigns@gmail.com>
2023-05-09 11:41:25 -0400

    Imported project from local drive

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './index.css'
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
+
+
+export default function App() {
+  const cards = data.map(item => {
+      return (
+          <Card
+              key={item.id}
+              {...item}
+              
+          />
+      )
+  })        
+  
+  return (
+      <div>
+          <Navbar />
+          <Hero />
+          <section className="cards-list">
+              {cards}
+          </section>
+      </div>
+  )
+}
+    
+
