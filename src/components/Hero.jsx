8825ce5d9a8a4a847123015ea91e416eb5c6013f
chaneldevelops <chanel.developsdesigns@gmail.com>
2023-05-10 23:50:06 -0400

    New hero image and new search color
    - added a new hero image
    - updated the search image/icon
    - changed the search bar activate border color

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,10 @@ import React from "react"
 export default function Hero() {
     return (
         <section className="hero">
-            <img src="../images/photo-grid.png" className="hero--photo" />
-            <h1 className="hero--header">Online Experiences</h1>
-            <p className="hero--text">Join unique interactive activities led by 
-            one-of-a-kind hosts—all without leaving home.</p>
+            <img src="../images/newest-grid.png" className="hero--photo" />
+            <h1 className="hero--header">Experiences</h1>
+            <p className="hero--text">Discover unique activities and experiences led by local experts with Airbnb Experiences, where you can explore your interests, 
+                learn new skills, and make unforgettable memories during your travels.</p>
         </section>
     )
 }
