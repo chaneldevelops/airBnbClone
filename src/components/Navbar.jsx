6ec76766852e755252301e2a6aaf49f9cd8c46d8
chaneldevelops <chanel.developsdesigns@gmail.com>
2023-05-11 00:30:13 -0400

    Search text update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <nav>
             <img src="./images/airbnb-logo.png" className="nav--logo" />
-            <input className="search" type="text" placeholder="Search.." />
+            <input className="search" type="text" placeholder="Search..." />
         </nav>
     )
 }
